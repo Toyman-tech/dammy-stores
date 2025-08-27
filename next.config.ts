f212e97b634248cbeb5bfd0d1127c4347c5f92e6
toyman-tech <kolatoyin94@gmail.com>
2025-08-27 14:37:53 +0100

    new commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable ESLint during builds; lint issues won't block builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
